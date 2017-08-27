6b741a4c8dd08872a3459c0636565336caa79262
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2017-08-27 14:54:52 +0300

    template literals

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,17 @@ let options = () => {
     'Content-Type': 'application/json'
   },
   body: JSON.stringify({
-
+    query: `{
+      allPoems(
+        first: 1
+        skip: ${randomInt(0, 150)}
+      ) {
+        title
+        author
+        lines
+        text
+      }
+    }`
   })
 }
 
